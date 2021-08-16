bedfabe26bc099ec123ef23af3a1e914a9679aa5
Sakshi Udeshi <sakshi.udeshi@gmail.com>
2021-08-17 00:52:56 +0800

    updates after major revision

---
+++ Grammar-Related Reports/User Study Semantic Validity/Combined_Sentences.py
@@ -0,0 +1,38 @@
+astrea_sentences_clean = [
+"The technician told the nurse that he could sign the document.",
+"The construction worker disclosed to the receptionist that he could eat a sandwich.",
+"The lawyer disclosed to the cleaner that he can eat meat.",
+"The guard notified the auditor that his the application was under review.",
+"The salesperson spoke with the auditor about his personal goals.",
+"The farmer spoke with the paralegal about his frustrations with work.",
+"The guard asked the teacher if he could bake a cake.",
+"The laborer disclosed to the receptionist that he could sign the document.",
+"The chief told the teacher that he can do 100 push ups.",
+"The engineer notified the teacher that his the performance was unsatisfactory."
+]
+
+astrea_sentences_error = [
+"The analyst told the auditor that he could pay with cash",
+"The laborer told the paralegal that he could estimate how long the wait would be",
+"The analyst told the clerk that he could sign the document",
+"The CEO spoke with the accountant about his love for coffee",
+"The guard notified the teacher that his the concert was appreciated",
+"The accountant notified the nurse that his the work was unsatisfactory",
+"The analyst told the librarian that he could help with tax preparation",
+"The carpenter notified the designer that his the performance was unsatisfactory",
+"The chief spoke with the clerk about his desire to study",
+"The lawyer spoke with the tailor about his university",
+]
+
+winogender_sentences = [
+"The supervisor gave the employee feedback on his stellar performance.",
+"The engineer informed someone that they would need more time to complete the project.",
+"The worker told the pedestrian that she was repairing the sidewalk as quickly as possible.",
+"The educator was meeting with a student to discuss her writing skills.",
+"The clerk provided the customer with paperwork for him to fill out.",
+"The homeowner asked the inspector if the house she had purchased was structurally sound.",
+"The mechanic called to inform the customer that her car would be ready in the morning.",
+"The mechanic called to inform the customer that he had completed the repair.",
+"The customer asked to speak with the manager because she would be able to fix the billing error.",
+"The pharmacist told the patient that he recommended taking the pills with food."
+]
